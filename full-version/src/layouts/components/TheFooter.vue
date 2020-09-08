444d58265c11a10d43baffa7522cfb160d6d484f
maxim krylov <m.krylov@antraks.ru>
2020-09-08 18:14:47 +0300

    add copyricht footer

--- full-version/src/layouts/components/TheFooter.vue
+++ full-version/src/layouts/components/TheFooter.vue
@@ -10,7 +10,11 @@
 
 <template functional>
     <footer class="the-footer flex-wrap justify-between" :class="classes">
-        <span>COPYRIGHT &copy; {{ new Date().getFullYear() }} <a href="/" target="_blank" rel="nofollow"></a> Антракс!</span>
+<!--        <span>COPYRIGHT &copy; {{ new Date().getFullYear() }} <a href="/" target="_blank" rel="nofollow"></a> Антракс!</span>-->
+      <router-link tag="div" to="/" class="vx-logo cursor-pointer mx-auto flex items-center">
+        <logo class="w-10 mr-4 fill-current text-primary" />
+        <span class="vx-logo-text text-primary">Коморсан</span>
+      </router-link>
         <span class="md:flex hidden items-center">
             <span></span>
             <feather-icon icon="HeartIcon" svgClasses="stroke-current text-danger w-6 h-6" class="ml-2" />
@@ -23,13 +27,21 @@
 
 <script>
 import BuyNow from '../../components/BuyNow.vue'
+import Logo                 from "./Logo.vue"
+
 
 export default {
     name: "the-footer",
     props: {
+        logo: {
+           type: String
+        },
         classes: {
             type: String,
         },
+    },
+    components: {
+      Logo
     },
     inject: {
       components:{
