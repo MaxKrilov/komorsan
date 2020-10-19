3472ae650910e13486e206e47ec35dd1cda6e8b2
maxim krylov <m.krylov@antraks.ru>
2020-10-19 17:54:22 +0300

    monitoring

--- src/store/monitoring-management/moduleMonitoringManagementActions.js
+++ src/store/monitoring-management/moduleMonitoringManagementActions.js
@@ -12,7 +12,7 @@ import getKitsStates from "@/http/requests/indication/sets/index.js"
 export default {
   // addItem({ commit }, item) {
   //   return new Promise((resolve, reject) => {
-  //     axios.post("/api/data-list/products/", {item: item})
+  //     axios.post("/api/data-list/", {item: item})
   //       .then((response) => {
   //         commit('ADD_ITEM', Object.assign(item, {id: response.data.id}))
   //         resolve(response)
@@ -49,33 +49,5 @@ export default {
         })
     })
   },
-  fetchUsers({ commit }) {
-    return new Promise((resolve, reject) => {
-      axios.get("/api/user-management/users")
-        .then((response) => {
-          commit('SET_USERS', response.data)
-          resolve(response)
-        })
-        .catch((error) => { reject(error) })
-    })
-  },
-  fetchUser(context, userId) {
-    return new Promise((resolve, reject) => {
-      axios.get(`/api/user-management/users/${userId}`)
-        .then((response) => {
-          resolve(response)
-        })
-        .catch((error) => { reject(error) })
-    })
-  },
-  removeRecord({ commit }, userId) {
-    return new Promise((resolve, reject) => {
-      axios.delete(`/api/user-management/users/${userId}`)
-        .then((response) => {
-          commit('REMOVE_RECORD', userId)
-          resolve(response)
-        })
-        .catch((error) => { reject(error) })
-    })
-  }
+
 }
--- src/store/monitoring-management/moduleMonitoringManagementMutations.js
+++ src/store/monitoring-management/moduleMonitoringManagementMutations.js
@@ -12,11 +12,11 @@ export default {
   SET_DEVICES_FROM_API(state, devices) {
     state.devices = devices
   },
-  SET_USERS(state, users) {
-    state.users = users
+  SET_DEVICES(state, devices) {
+    state.devices = devices
   },
   REMOVE_RECORD(state, itemId) {
-      const userIndex = state.users.findIndex((u) => u.id == itemId)
-      state.users.splice(userIndex, 1)
+      const userIndex = state.devices.findIndex((u) => u.id == itemId)
+      state.devices.splice(userIndex, 1)
   },
 }
